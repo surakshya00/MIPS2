13bc37d460f98ba6784f89510fd7a9d01aa7aa1b
surakshyaa <83180728+surakshyaa@users.noreply.github.com>
2021-11-17 17:39:25 -0500

    completed the code to check if its null or space or end of line

--- project2.s
+++ project2.s
@@ -15,12 +15,14 @@ main:
         li $t9, 0                  #to make a condition of true or false to check if the character was found or not
 
 .globl main 
-
 Char_found:
         lb $a0,($a1)       #loads the single byte into $a1
         addi $a1, $a1, 1     #immediately increases the index by 1 after it is loaded
         
         beq $a0, 0, Non_char      #if the character is null then it branches to Non_char label
-        beq $a0, 10, Non_char      #if the character is spaace cause the ascii val of space is 10, then it branches to Non_char
+        beq $a0, 32, Char_found      #if the character is space cause the ascii val of space is 32, then it keeps on looping
+        beq $a0, 10, Non_char      #if the character is end of line then it branches to non char label again
         
+ .globl main       
+ Non_char:
         
