ad4c255b6f4c451a86315e5ca2dff80c86e29d1d
surakshyaa <83180728+surakshyaa@users.noreply.github.com>
2021-11-17 17:07:50 -0500

    loaded the address of user input and set t9 to be 0 to make a condition of characters found or not

--- project2.s
+++ project2.s
@@ -1,13 +1,17 @@
 .data
         invalid_input: .asciiz "Not recognized"     #prints not recognized if the input is not valid 
-        user_input: .space 1001
-        required: .space 5
+        user_input: .space 1001                      #reserves space for 1000bytes of character 
+        required: .space 5                            #reserves space for 4 sub characters 
 .text
 
+.globl main
 main:
-        li $v0, 8
-        la $a0, user_input
-        li $a1, 1001
+        li $v0, 8                 #prepares for user input
+        la $a0, user_input         #loads the address of the first byte
+        li $a1, 1001              #max length of the characters
         syscall
+        
+        la $a1, user_input          #loads the address of a1 to user_input to check all the values
+        li $t9, 0                  #to make a condition of true or false to check if the character was found or not
 
         
