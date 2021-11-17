9aeb455c19c85a2de33a408764e1c2dd3d918e61
surakshyaa <83180728+surakshyaa@users.noreply.github.com>
2021-11-17 12:40:11 -0500

    reserved the space for 4 characters

--- project2.s
+++ project2.s
@@ -1,5 +1,9 @@
 .data
         invalid_input: .asciiz "Not recognized"     #prints not recognized if the input is not valid 
         user_input: .space 1001
+        required: .space 5
 .text
+
+main:
+
         
